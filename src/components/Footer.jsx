58d8b50b2c471b7cce63216292ec87141901aeb2
Alex <alex.holder@bigfootds.com>
2025-02-19 20:32:50 +1100

    feat: pages with layout in place

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export function Footer(){
+
+	return(
+		<footer>
+			Copyright Alex {new Date(Date.now()).getFullYear()}
+		</footer>
+	)
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "./Navbar";
+
+export function Header(){
+
+	return(
+		<header>
+			<h1>Alex&apos;s Super Cool Portfolio!</h1>
+
+			<Navbar />
+		</header>
+	)
+}
